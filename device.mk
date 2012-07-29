8e9328c3201dfe159d2a42b00a20eb363a4463d5
Rob Beane <robbeane@gmail.com>
2012-07-29 17:01:11 -0400

    ace: Changes for building JellyBean
    Change-Id: If8e8fda5ec4c19ca0b7fdf6cd1ede67b7402ae00

--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PACKAGE_OVERLAYS += device/htc/ace/overlay
 
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
 
 # gsm config xml file
 PRODUCT_COPY_FILES += \
@@ -179,5 +179,5 @@ $(call inherit-product, device/htc/msm7x30-common/msm7x30.mk)
 # htc audio settings
 $(call inherit-product, device/htc/ace/media_htcaudio.mk)
 
-$(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
